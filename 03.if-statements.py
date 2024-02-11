d151264d4d46705532b8cdbf9fb1bf1404406478
Victor Almanzar <105403994+victor-almanzar@users.noreply.github.com>
2024-02-11 17:34:56 -0500

    Add if-statements and conditional tests examples

---
+++ 03.if-statements.py
@@ -0,0 +1,50 @@
+# Example
+
+cars = ["audi", "bmw", "subaru", "toyota"]
+
+for car in cars:
+    if car == "bmw":
+        print(car.upper())
+    else:
+        print(car.title())
+
+print("")
+
+# Conditional Tests
+
+car = "bmw"
+print(car == "bmw")  # True
+car = "audi"
+print(car == "bmw")  # False
+car = "Audi"
+print(car == "audi")  # False (Python is case sensitive)
+print(car.lower() == "audi")  # True
+
+print("")
+
+# Checking for Inequality
+
+requested_topping = "mushrooms"
+if requested_topping != "anchovies":
+    print("Hold the anchovies!")
+
+print("")
+
+# Numerical Comparisons
+
+age = 18
+print(age == 18)  # True
+
+answer = 17
+if answer != 42:
+    print("Wrong. Try again!")
+
+age = 19
+print(age < 21)  # True
+print(age <= 21)  # True
+print(age > 21)  # False
+print(age >= 21)  # False
+
+print("")
+
+# Checking Multiple Conditions
